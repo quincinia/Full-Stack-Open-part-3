2b3a3e8f596a3cd4e49d4f6aefef5482275fa849
quincinia <eanj13@gmail.com>
2021-12-16 15:13:13 -0800

    prepping files for deployment (heroku)

--- part3/index.js
+++ part3/index.js
@@ -113,7 +113,7 @@ app.post("/api/persons", (req, res) => {
     res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is live on port ${PORT}`)
     console.log(`http://localhost:${PORT}`)
