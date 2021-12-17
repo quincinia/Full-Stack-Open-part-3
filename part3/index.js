e74e00c3c3b80f28df318caadcb711b5c6eb3db2
quincinia <eanj13@gmail.com>
2021-12-16 20:19:19 -0800

    now serving frontend files

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(
     morgan("tiny", {
         skip: (req, res) => req.method === "POST",
